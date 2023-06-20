15c5899a734a8a6936131550570104b80f1e658f
SapuLid1 <64718440+SapuLid1@users.noreply.github.com>
2023-06-20 19:50:10 +0800

    input range, CSS, JS for picker.html

--- js/clr_picker.js
+++ js/clr_picker.js
@@ -1,10 +1,21 @@
 function myColor(){
-    var red =document.getElementById('red').value;
-    var green =document.getElementById('green').value;
-    var blue =document.getElementById('blue').value;
-    document.getElementById('rgbCode').innerHTML = "rgb(" +red+ " ," +green+ " ," +blue+ " )";
-   
-    document.getElementById('hexCode').innerHTML = "#" +hexRed+ +hexGreen+ +hexBlue;
-    document.body.style.backgroundColor = "rgb(" +red+ " ," +green+ " ," +blue+ " )";
+    var red =parseInt(document.getElementById('red').value);
+    var green =parseInt(document.getElementById('green').value);
+    var blue =parseInt(document.getElementById('blue').value);
+    document.getElementById('rgbCode').innerHTML = "rgb(" + red + " ," + green + " ," + blue + " )";
+    var hexRed = red.toString(16);
+    var hexGreen = green.toString(16);
+    var hexBlue = blue.toString(16); 
+    if(red < 17){
+        hexRed ="0" + hexRed;
+    }
+    if(green < 17){
+        hexGreen ="0" + hexGreen;
+    }
+    if(blue < 17){
+        hexBlue ="0" + hexBlue;
+    }
+    document.getElementById('hexCode').innerHTML = "#" + hexRed + + hexGreen + + hexBlue;
+    document.body.style.backgroundColor = "rgb(" + red + " ," + green + " ," + blue + " )";
     
 }
