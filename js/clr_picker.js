fd620fa1c3c5679b05e38f85d04fb975f28ddbf5
SapuLid1 <64718440+SapuLid1@users.noreply.github.com>
2023-06-21 11:36:20 +0800

    Change rgbCode&hexCode box color, changing "var" to "const"

--- js/clr_picker.js
+++ js/clr_picker.js
@@ -1,21 +1,21 @@
-function myColor(){
-    var red =parseInt(document.getElementById('red').value);
-    var green =parseInt(document.getElementById('green').value);
-    var blue =parseInt(document.getElementById('blue').value);
+function myColor() {
+    const red = parseInt(document.getElementById('red').value);
+    const green = parseInt(document.getElementById('green').value);
+    const blue = parseInt(document.getElementById('blue').value);
     document.getElementById('rgbCode').innerHTML = "rgb(" + red + " ," + green + " ," + blue + " )";
-    var hexRed = red.toString(16);
-    var hexGreen = green.toString(16);
-    var hexBlue = blue.toString(16); 
-    if(red < 17){
-        hexRed ="0" + hexRed;
+    const hexRed = red.toString(16);
+    const hexGreen = green.toString(16);
+    const hexBlue = blue.toString(16);
+    if (red < 10) {
+        hexRed = "0" + hexRed;
     }
-    if(green < 17){
-        hexGreen ="0" + hexGreen;
+    if (green < 10) {
+        hexGreen = "0" + hexGreen;
     }
-    if(blue < 17){
-        hexBlue ="0" + hexBlue;
+    if (blue < 10) {
+        hexBlue = "0" + hexBlue;
     }
-    document.getElementById('hexCode').innerHTML = "#" + hexRed + + hexGreen + + hexBlue;
+    document.getElementById('hexCode').innerHTML = "#" + hexRed + hexGreen + hexBlue;
     document.body.style.backgroundColor = "rgb(" + red + " ," + green + " ," + blue + " )";
-    
+
 }
