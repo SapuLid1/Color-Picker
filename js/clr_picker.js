a0fbb2c16fc044012a089d37a912342ea3fcca1a
SapuLid1 <64718440+SapuLid1@users.noreply.github.com>
2023-06-21 12:57:45 +0800

    change line 6-8 from const to Let

--- js/clr_picker.js
+++ js/clr_picker.js
@@ -3,18 +3,18 @@ function myColor() {
     const green = parseInt(document.getElementById('green').value);
     const blue = parseInt(document.getElementById('blue').value);
     document.getElementById('rgbCode').innerHTML = "rgb(" + red + " ," + green + " ," + blue + " )";
-    const hexRed = red.toString(16);
-    const hexGreen = green.toString(16);
-    const hexBlue = blue.toString(16);
-    if (red < 10) {
+    let hexRed = red.toString(16);
+    let hexGreen = green.toString(16);
+    let hexBlue = blue.toString(16);
+        if (red < 10) {
         hexRed = "0" + hexRed;
-    }
-    if (green < 10) {
+        }
+        if (green < 10) {
         hexGreen = "0" + hexGreen;
-    }
-    if (blue < 10) {
+        }
+        if (blue < 10) {
         hexBlue = "0" + hexBlue;
-    }
+        }
     document.getElementById('hexCode').innerHTML = "#" + hexRed + hexGreen + hexBlue;
     document.body.style.backgroundColor = "rgb(" + red + " ," + green + " ," + blue + " )";
 
